9f3afb91b115a029b7088f866a40d7182ec1cabc
Endre Ulberg <endre.ulberg@gmail.com>
2023-05-13 12:03:14 +0200

    Updates processing to a more performant solution
    Adds adjustment to only update when the timestamp of the new product from KasseLapp is newer than the last update on our end.
    
    Renaming
    
    lint

--- src/pages/api/cron/processDataFromKasseLappenApiPayloads.ts
+++ src/pages/api/cron/processDataFromKasseLappenApiPayloads.ts
@@ -1,52 +1,89 @@
-import { EanResponeDtos } from "@prisma/client";
+import type { EanResponeDtos } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "~/server/db";
-import { NextApiRequest, NextApiResponse } from "next";
 import { kasseLappEANResponseDto } from "./storeDataFromKasseLappenAPI";
 
-const upsertRecord = async (productsFromKassaLapp: EanResponeDtos) => {
-  const validatedProducts = await kasseLappEANResponseDto.parseAsync(
-    productsFromKassaLapp.payload
-  );
+const upsertRecords = async (productsFromKassaLapp: EanResponeDtos[]) => {
+  const productsToUpdate = [];
+  const productsToCreate = [];
+  const productsToBeCreated: string[] = [];
 
-  for await (const validatedProduct of validatedProducts.data.products) {
-    await prisma.product.upsert({
+  const productInformation = (await prisma.product.findMany()).reduce((a, product) => ({ ...a, [`${product.ean}_${product.store}`]: product.updatedAt }), {})
+
+  for (const productFromKassaLapp of productsFromKassaLapp) {
+
+    const { data: validatedPayload } = kasseLappEANResponseDto.parse(
+      productFromKassaLapp.payload
+    )
+
+    for (const product of validatedPayload.products) {
+      const ean = validatedPayload.ean
+      const productToInsertToDb = {
+        ean,
+        name: product.name,
+        currentPrice: product.current_price.price,
+        store: product.store.name,
+        url: product.image,
+        extraData: "",
+      }
+
+      const key = `${validatedPayload.ean}_${product.store.name}`
+      const productUpdatedAt = new Date(product.updated_at)
+
+      if (key in productInformation) {
+        const internalProductUpdatedAt = new Date(productInformation[key as keyof typeof productInformation])
+
+        if (productUpdatedAt > internalProductUpdatedAt) {
+          productsToUpdate.push(productToInsertToDb);
+        }
+
+        continue
+      }
+
+      if (!productsToBeCreated.includes(key)) {
+        productsToBeCreated.push(key)
+        productsToCreate.push(productToInsertToDb)
+      }
+    }
+  }
+
+  await prisma.product.createMany({
+    data: productsToCreate
+  })
+  for (const productToUpdate of productsToUpdate) {
+    await prisma.product.update({
       where: {
         ean_store: {
-          ean: validatedProducts.data.ean,
-          store: validatedProduct.store.name,
-        },
+          ean: productToUpdate.ean,
+          store: productToUpdate.store
+        }
       },
-      update: { currentPrice: validatedProduct.current_price.price },
-      create: {
-        ean: validatedProducts.data.ean,
-        //TODO Trim the name properly
-        name: validatedProduct.name,
-        currentPrice: validatedProduct.current_price.price,
-        store: validatedProduct.store.name,
-        url: validatedProduct.image,
-        // put the extra info from name, and other fields, onto this one.
-        extraData: "",
-      },
-    });
-
-    await prisma.eanResponeDtos.update({
-      where: { id: productsFromKassaLapp.id },
-      data: { processed: true },
-    });
+      data: productToUpdate
+    })
   }
-};
+}
 
 async function PUT(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "PUT") {
     return res.status(405).json({ message: "Method not allowed" });
   }
+
   const eanResponseDtos = await prisma.eanResponeDtos.findMany({
-    take: 3,
-    where: { processed: false },
+    where: {
+      processed: false
+    }
   });
-  for await (const eanResponseDto of eanResponseDtos) {
-    await upsertRecord(eanResponseDto);
-  }
+
+  await upsertRecords(eanResponseDtos);
+
+  await prisma.eanResponeDtos.updateMany({
+    where: {
+      id: { in: eanResponseDtos.map(eRD => eRD.id) },
+    },
+    data: {
+      processed: true
+    }
+  });
   return res.status(200).json({ message: "Success!" });
 }
 export default PUT;
