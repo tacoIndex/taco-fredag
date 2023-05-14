4905eeaa7497dd99796bae9003bd9d468ef390f7
FavritEndre <endre@favrit.com>
2023-05-14 22:19:14 +0200

    Adds Incremental Service Side Regeneration
    nice
    
    nice
    
    p
    
    cleanupg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
-import { type NextPage } from "next";
+import type { Product } from "@prisma/client";
+import { prisma } from "~/server/db";
 import Head from "next/head";
 import Image from "next/image";
-import { api } from "~/utils/api";
+import type { NextPage } from "next";
+
 const dateFormatter = Intl.DateTimeFormat("nb-NO", {
   weekday: "long",
   day: "2-digit",
@@ -9,8 +11,7 @@ const dateFormatter = Intl.DateTimeFormat("nb-NO", {
   year: "2-digit",
 });
 
-const Home: NextPage = () => {
-  const { data: products } = api.product.getUniqueProduct.useQuery();
+const Home: NextPage<{ products: Product[], lastUpdated:  number }> = ({ products, lastUpdated }) => {
 
   return (
     <>
@@ -32,48 +33,69 @@ const Home: NextPage = () => {
             <span className="text-3xl">🌮</span>
           </div>
           <article className="flex flex-col">
-            <div className="rounded-lg bg-[#FBE0C7] p-8 mb-2 xl:mb-0">
-            <p className=" mb-6 text-[#33373D]">
-              <strong>I handlekurven</strong>
-            </p>
-            <div className="grid grid-cols-2 gap-2 md:grid-cols-5 md:gap-3">
-              {products?.map((product) => {
-                return (
-                  <div
-                    key={product.id}
-                    className="flex h-40 w-32 flex-col items-center gap-1 rounded-md bg-white p-2"
-                  >
-                    <div className="h-20 w-24">
-                      <div className="relative h-full w-full">
-                        <Image
-                          alt={product.name}
-                          src={product.url}
-                          placeholder="empty"
-                          fill
-                          style={{objectFit: "contain"}}
-                        />
+            <div className="mb-2 rounded-lg bg-[#FBE0C7] p-8 xl:mb-0">
+              <p className=" mb-6 text-[#33373D]">
+                <strong>I handlekurven</strong>
+              </p>
+              <div className="grid grid-cols-2 gap-2 md:grid-cols-5 md:gap-3">
+                {products?.map((product) => {
+                  return (
+                    <div
+                      key={product.id}
+                      className="flex h-40 w-32 flex-col items-center gap-1 rounded-md bg-white p-2"
+                    >
+                      <div className="h-20 w-24">
+                        <div className="relative h-full w-full">
+                          <Image
+                            alt={product.name}
+                            src={product.url}
+                            placeholder="empty"
+                            fill
+                            style={{ objectFit: "contain" }}
+                          />
+                        </div>
                       </div>
+                      <p className="text-center text-xs text-[#454D60]">
+                        {product.extraData}
+                      </p>
+                      <p className=" text-ellipsis	text-center	 text-sm">
+                        {product.name}
+                      </p>
                     </div>
-                    <p className="text-center text-xs text-[#454D60]">
-                      {product.extraData}
-                    </p>
-                    <p className=" text-ellipsis	text-center	 text-sm">
-                      {product.name}
-                    </p>
-                  </div>
-                );
-              })}
-            </div>
+                  );
+                })}
+              </div>
             </div>
             <p className="self-end ">
-            Sist oppdatert: {dateFormatter.format(products?.at(0)?.updatedAt)}
-          </p>
+              Sist oppdatert:
+              {dateFormatter.format(
+                new Date(lastUpdated)
+              )}
+            </p>
           </article>
-
         </section>
       </main>
     </>
   );
 };
+
+export async function getStaticProps() {
+  const products = await prisma.product.findMany();
+  const uniqueProducts = products.filter(
+    (product, index, self) =>
+      index === self.findIndex((t) => t.ean === product.ean)
+  );
+  const fixedPaylods: unknown = JSON.parse(JSON.stringify(uniqueProducts))
+
+  const lastUpdated = Math.max(...products.map(product => product.updatedAt.getTime()))
+
+  return {
+    props: {
+      products: fixedPaylods,
+      lastUpdated
+    },
+    revalidate: 43200,
+  };
+}
 
 export default Home;
